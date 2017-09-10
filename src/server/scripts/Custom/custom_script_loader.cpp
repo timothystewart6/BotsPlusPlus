fd2ffa003c54a1e60ab88763dd80ef1a6fd3bf73
Timothy Stewart <timothystewart6@hotmail.com>
2017-09-10 15:01:20 -0500

    wip:beastmaster

--- src/server/scripts/Custom/custom_script_loader.cpp
+++ src/server/scripts/Custom/custom_script_loader.cpp
@@ -42,7 +42,7 @@ void AddSC_Player_Boa();
 void AddSC_Boss_Announcer();
 void AddSC_accountachievement();
 void AddSC_custom_CongratsOnLevel();
-void AddSC_Npc_Beastmaster();
+void AddSC_Beastmaster_NPC();
 
 void AddCustomScripts()
 {
@@ -70,7 +70,5 @@ void AddCustomScripts()
     AddSC_Boss_Announcer();
     AddSC_accountachievement();
     AddSC_custom_CongratsOnLevel();
-    AddSC_Npc_Beastmaster();    
-    
-
+    AddSC_Beastmaster_NPC();
 }
