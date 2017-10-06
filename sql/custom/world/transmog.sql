bbce99633cb41eb636f3a7c13bda8e7fb349b80f
Timothy Stewart <timothystewart6@hotmail.com>
2017-10-06 17:40:15 -0500

    fix(transmog): Uppdated sql

--- sql/custom/world/transmog.sql
+++ sql/custom/world/transmog.sql
@@ -1,6 +1,6 @@
-SET @ENTRY :=190010;
-SET @NAME :="Warpweaver";
-SET @SUBNAME :="Transmogrifier";
+SET @NAME := "Warpweaver";
+SET @SUBNAME := "Transmogrifier";
+SET @ENTRY := 190010;
 
 REPLACE INTO `creature_template` (`entry`, `modelid1`, `modelid2`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `scale`, `rank`, `dmgschool`, `baseattacktime`, `rangeattacktime`, `unit_class`, `unit_flags`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `AIName`, `MovementType`, `InhabitType`, `HoverHeight`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `flags_extra`, `ScriptName`) VALUES
 (@ENTRY, 19646, 0, @NAME, @SUBNAME, NULL, 0, 80, 80, 2, 35, 1, 1, 0, 0, 2000, 0, 1, 0, 7, 138936390, 0, 0, 0, '', 0, 3, 1, 0, 0, 1, 0, 0, 'Creature_Transmogrify');
