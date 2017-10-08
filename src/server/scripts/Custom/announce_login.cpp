213d0dc4cf315caa23d33f41b39be37d0c443c59
Timothy Stewart <timothystewart6@hotmail.com>
2017-10-08 10:30:48 -0500

    fix(custom): Removed faction in annoucement

--- src/server/scripts/Custom/announce_login.cpp
+++ src/server/scripts/Custom/announce_login.cpp
@@ -5,11 +5,11 @@ public:
 
     void OnLogin(Player* player, bool /*loginFirst*/)
     {
-
         std::ostringstream ss;
             ss << "|cff3DAEFF[ Login Announcer ]|cffFFD800 : Player|cff4CFF00 " << player->GetName() << " |cffFFD800Is Online.";
         sWorld->SendServerMessage(SERVER_MSG_STRING, ss.str().c_str());
-    };
+    }
+};
 
 void AddSC_announce_login()
 {
