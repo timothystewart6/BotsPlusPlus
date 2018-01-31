63483e8eab5745e576e95251905844b8b54d7f2d
Timothy Stewart <timothystewart6@hotmail.com>
2018-01-30 20:07:13 -0600

    feat(npc bots): Create an spawn all npc bots in Stormwind

---
+++ sql/custom/world/spawned_npc_bots_sw.sql
@@ -0,0 +1,54 @@
+-- priest in SW
+INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `VerifiedBuild`)
+VALUES
+	(15130057, 70184, 0, 0, 0, 1, 1, 0, 0, -8824.76, 636.576, 94.2409, 2.20231, 300, 0, 0, 10080, 8814, 0, 0, 0, 0, 0);
+
+-- shaman in SW
+INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `VerifiedBuild`)
+VALUES
+	(15130058, 70222, 0, 0, 0, 1, 1, 0, 0, -8823.54, 637.432, 94.2346, 2.23961, 300, 0, 0, 12600, 3994, 0, 0, 0, 0, 0);
+
+-- hunter in SW
+INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `VerifiedBuild`)
+VALUES
+	(15130059, 70219, 0, 0, 0, 1, 1, 0, 0, -8822.12, 638.168, 94.2298, 2.28516, 300, 0, 0, 12600, 3994, 0, 0, 0, 0, 0);
+
+-- warrior in SW
+INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `VerifiedBuild`)
+VALUES
+	(15130060, 70123, 0, 0, 0, 1, 1, 0, 0, -8819.3, 629.333, 94.0133, 5.45346, 300, 0, 0, 12600, 0, 0, 0, 0, 0, 0);
+
+-- shaman in SW
+INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `VerifiedBuild`)
+VALUES
+	(15130062, 70076, 0, 0, 0, 1, 1, 0, 0, -8816.76, 631.694, 94.2294, 5.38474, 300, 0, 0, 10080, 8814, 0, 0, 0, 0, 0);
+
+-- paladin in SW
+INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `VerifiedBuild`)
+VALUES
+	(15130068, 70023, 0, 0, 0, 1, 1, 0, 0, -8818.07, 630.558, 94.1282, 5.40163, 300, 0, 0, 12600, 3994, 0, 0, 0, 0, 0);
+
+-- mage in SW
+INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `VerifiedBuild`)
+VALUES
+	(15130076, 70024, 0, 0, 0, 1, 1, 0, 0, -8816.42, 635.219, 94.2291, 0.706513, 300, 0, 0, 10080, 8814, 0, 0, 0, 0, 0);
+
+-- rogue in SW
+INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `VerifiedBuild`)
+VALUES
+	(15130121, 70211, 0, 0, 0, 1, 1, 0, 0, -8818.35, 638.423, 94.2283, 0.622498, 300, 0, 0, 12600, 0, 0, 0, 0, 0, 0);
+
+-- druid in SW
+INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `VerifiedBuild`)
+VALUES
+	(15130095, 70176, 0, 0, 0, 1, 1, 0, 0, -8817.33, 636.72, 94.2289, 0.796824, 300, 0, 0, 12600, 3994, 0, 0, 0, 0, 0);
+
+-- dk in SW
+INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `VerifiedBuild`)
+VALUES
+	(15130105, 70239, 0, 0, 0, 1, 1, 0, 0, -8810.14, 619.196, 94.9965, 2.24748, 300, 0, 0, 12600, 0, 0, 0, 0, 0, 0);
+
+-- bm in SW
+INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `VerifiedBuild`)
+VALUES
+	(15130107, 70301, 0, 0, 0, 1, 1, 0, 0, -8805.89, 621.839, 94.9007, 2.17483, 300, 0, 0, 13033, 0, 0, 0, 0, 0, 0);
