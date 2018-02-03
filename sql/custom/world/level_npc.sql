27e200d280b220e775244b6d80758830ff66ee75
Timothy Stewart <timothystewart6@hotmail.com>
2018-02-03 16:01:32 -0600

    feat(Level NPC): Added Maiden of the Stars, a level master who can grant you 10 levels or boost up to 80

--- sql/custom/world/level_npc.sql
+++ sql/custom/world/level_npc.sql
@@ -1,19 +1,15 @@
 
 SET
 @ENTRY = 601000,
-@NAME = "Level",
-@SUBNAME = "Level Vendor";
+@NAME = "Maiden of the Stars",
+@SUBNAME = "Level Master";
 
 REPLACE INTO `creature_template` (entry,modelid1,name,subname,minlevel,maxlevel,faction,scale,npcflag,InhabitType,ScriptName,AIName,iconname) VALUES
-(@ENTRY,28970,@NAME,@SUBNAME,80,80,35,1,35,3,"Level_NPC","SmartAI","Directions");
-
-
--- INSERT INTO `creature_template` VALUES ('14', '0', '0', '0', '0', '0', '21572', '0', '0', '0', 'Level Npc', '', 'Directions', '0', '71', '71', '0', '35', '35', '1', '1', '1.14286', '1.25', '0', '0', '0', '0', '0', '1', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '7', '138936390', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'SmartAI', '0', '3', '1', '1.56', '1.56', '1.56', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '2', 'vote_NPC', '1');
-
+(@ENTRY,28777,@NAME,@SUBNAME,80,80,35,.15,35,3,"Level_NPC","SmartAI","Directions");
 
 
 REPLACE INTO creature (guid, id, map, spawnMask, phaseMask, modelid, equipment_id, position_x, position_y, position_z, orientation, spawntimesecs, spawndist, currentwaypoint, curhealth, curmana, MovementType, npcflag, unit_flags, dynamicflags) VALUE
 -- Orgimmar
-('15200100', @ENTRY, '1', '1', '1', '0', '0', '1574.658569', '-4401.979492', '7.172108', '5.661114', '300', '0', '0', '1', '0', '0', '0', '0', '0'),
+('15200100', @ENTRY, '1', '1', '1', '0', '0', '1570.647095', '-4407.840332', '7.654320', '5.810328', '300', '0', '0', '1', '0', '0', '0', '0', '0'),
 -- Stormwind
-('15200101', @ENTRY, '0', '1', '1', '0', '0', '-8839.585938', '648.703979', '95.914322', '3.888847', '300', '0', '0', '1', '0', '0', '0', '0', '0');
+('15200101', @ENTRY, '0', '1', '1', '0', '0', '-8839.855469', '648.560791', '96.271896', '3.892737', '300', '0', '0', '1', '0', '0', '0', '0', '0');
