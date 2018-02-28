185c49aa2bbac062eb8e6922343f35815be8ed3a
Timothy Stewart <timothystewart6@hotmail.com>
2018-02-27 20:18:49 -0600

    feat(account companions): Added account companion script

--- src/server/scripts/Custom/account_companions.cpp
+++ src/server/scripts/Custom/account_companions.cpp
@@ -1,76 +1,58 @@
-#pragma clang diagnostic ignored "-Wunused-variable"
-/*
-<--------------------------------------------------------------------------->
-- Developer(s): Lille Carl, Grim/Render
-- Complete: %100
-- ScriptName: 'AccountCompanions'
-- Comment: Tested and Works.
-- Orginial Creator: Lille Carl
-- Edited: Render/Grim
-<--------------------------------------------------------------------------->
-*/
-
+#include "Player.h"
 
 class AccountCompanions : public PlayerScript
 {
-	static const bool limitrace = true; // This set to true will only achievements from chars on the same team, do what you want. NOT RECOMMANDED TO BE CHANGED!!!
-	static const bool limitlevel = true; // This checks the player's level and will only add achievements to players of that level.
-	int minlevel = 80; // It's set to players of the level 60. Requires limitlevel to be set to true.
-	int setlevel = 1; // Dont Change
-
+    static const bool limitrace = false; // This set to true will only learn mounts from chars on the same team, do what you want.
 public:
 	AccountCompanions() : PlayerScript("AccountCompanions") { }
 
-	void OnLogin(Player* pPlayer, bool)
+    void OnLogin(Player* pPlayer, bool first_login)
 	{
-		std::vector<uint32> Guids;
-		QueryResult result1 = CharacterDatabase.PQuery("SELECT guid, race FROM characters WHERE account = %u", pPlayer->GetSession()->GetAccountId());
-		if (!result1)
-			return;
+        std::vector<uint32> Guids;
+        QueryResult result1 = CharacterDatabase.PQuery("SELECT guid, race FROM characters WHERE account = %u", pPlayer->GetSession()->GetAccountId());
+        if (!result1)
+            return;
 
-		do
-		{
-			Field* fields = result1->Fetch();
+        do
+        {
+            Field* fields = result1->Fetch();
 
-			uint32 guid = fields[0].GetUInt32();
-			uint32 race = fields[1].GetUInt8();
+            uint32 guid = fields[0].GetUInt32();
+            uint32 race = fields[1].GetUInt8();
 
-			if ((Player::TeamForRace(race) == Player::TeamForRace(pPlayer->getRace())) || !limitrace)
-				Guids.push_back(result1->Fetch()[0].GetUInt32());
+            if ((Player::TeamForRace(race) == Player::TeamForRace(pPlayer->getRace())) || !limitrace)
+                Guids.push_back(result1->Fetch()[0].GetUInt32());
 
-		} while (result1->NextRow());
+        } while (result1->NextRow());
 
-		std::vector<uint32> Spell;
+          std::vector<uint32> Spells;
 
-		for (auto& i : Guids)
-		{
-			QueryResult result2 = CharacterDatabase.PQuery("SELECT spell FROM character_spell WHERE guid = %u", i);
-			if (!result2)
-				continue;
+           for (auto& i : Guids)
+           {
+               QueryResult result2 = CharacterDatabase.PQuery("SELECT spell FROM character_spell WHERE guid = %u", i);
+               if (!result2)
+                   continue;
 
-			do
-			{
-				Spell.push_back(result2->Fetch()[0].GetUInt32());
-			} while (result2->NextRow());
-		}
+               do
+               {
+                   Spells.push_back(result2->Fetch()[0].GetUInt32());
+               } while (result2->NextRow());
+           }
 
-		std::vector<uint32> CompanionSpell;
+		std::vector<uint32> CompanionSpells;
 
 
         for (auto& i : Spell)
 		{
-			QueryResult result2 = WorldDatabase.PQuery("SELECT spellid_2 FROM item_template WHERE spellid_2 = %u and class = 6", i);
+			QueryResult result3 = WorldDatabase.PQuery("SELECT spellid_2 FROM item_template WHERE spellid_2 = %u and class = 6", i);
 			if (!result2)
 				continue;
 
 			do
 			{
-				CompanionSpell.push_back(result2->Fetch()[0].GetUInt32());
-			} while (result2->NextRow());
+				CompanionSpells.push_back(result3->Fetch()[0].GetUInt32());
+			} while (result3->NextRow());
 		}
-
-
-
 
 		for (auto& i : CompanionSpell)
 		{
