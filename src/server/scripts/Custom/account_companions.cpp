64f81c783b2d97156a772526536255aefb55012b
Timothy Stewart <timothystewart6@hotmail.com>
2018-02-28 19:59:17 -0600

    fix(account companions): Fixed account companions

--- src/server/scripts/Custom/account_companions.cpp
+++ src/server/scripts/Custom/account_companions.cpp
@@ -42,7 +42,7 @@ public:
 		for (auto& i : Spells)
 		{
 			auto sSpell = sSpellStore.LookupEntry(i);
-			 if (sSpell->Effect[0] == SPELL_EFFECT_SUMMON && sSpell->EffectDismissPet[0])
+			 if (sSpell->Effect[0] == SPELL_EFFECT_SUMMON && minion->m_Properties->Type == SUMMON_TYPE_MINIPET)
                             pPlayer->LearnSpell(sSpell->Id, false);
 		}
 	}
