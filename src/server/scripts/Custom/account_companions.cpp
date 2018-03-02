cfd5ad8656199e4a99abb00212117a0c8ef3b33f
Timothy Stewart <timothystewart6@hotmail.com>
2018-03-01 19:23:43 -0600

    fix(account companions): Fixed account companions

--- src/server/scripts/Custom/account_companions.cpp
+++ src/server/scripts/Custom/account_companions.cpp
@@ -43,7 +43,7 @@ public:
 		for (auto& i : Spells)
 		{
 			auto sSpell = sSpellStore.LookupEntry(i);
-			 if (sSpell->Effect[0] == SPELL_EFFECT_SUMMON && m_spellInfo->HasAttribute(SPELL_ATTR1_DISMISS_PET))
+			 if (sSpell->Effect[0] == SPELL_EFFECT_SUMMON && sSpell->HasAttribute(SPELL_ATTR1_DISMISS_PET))
                             pPlayer->LearnSpell(sSpell->Id, false);
 		}
 	}
